5b8264e58cf14ab5885aa5f41e1f289f94b6c8c4
Cneale92 <connorneale92@gmail.com>
2024-08-06 22:36:32 -0500

    tried to fix error with schema creating tables

--- main/db/schema.sql
+++ main/db/schema.sql
@@ -8,14 +8,13 @@ CREATE DATABASE employee_db;
 -- Creates the three tables for our employee_db
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    -- department_id will reference the id in the department table and links the two tables together
     department_id INTEGER NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
@@ -24,11 +23,9 @@ CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    -- department_id property references the id from the role table and links the two tables together
     role_id INTEGER NOT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    -- manager_id  property references the id from this table
     manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
